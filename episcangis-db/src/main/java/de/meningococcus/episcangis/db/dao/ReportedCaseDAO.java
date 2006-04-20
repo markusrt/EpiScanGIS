874b0fb320ffca62bfca27369235f6dd8189c535
Markus Reinhardt <tuxxilla@gmail.com>
2006-04-20 13:25:25 +0000

    ADD: method to call SQL function episcangis_count_cases_per_area_attribute
    git-svn-id: svn://localhost/episcangis/trunk@37 878930ed-052c-0410-bf3c-f10d0cba850a

--- episcangis-db/src/main/java/de/meningococcus/episcangis/db/dao/ReportedCaseDAO.java
+++ episcangis-db/src/main/java/de/meningococcus/episcangis/db/dao/ReportedCaseDAO.java
@@ -17,22 +17,45 @@ import de.meningococcus.episcangis.db.model.ReportedCase;
 public interface ReportedCaseDAO
 {
   /**
-   * @param areaTier depth of AreaTypes in type hierarchie to match this case on
-   * @param caseTypeId return cases by CaseType
-   * @param from earliest reportDate  
-   * @param to latest reportDate
+   * @param areaTier
+   *          depth of AreaTypes in type hierarchie to match this case on
+   * @param caseTypeId
+   *          return cases by CaseType
+   * @param from
+   *          earliest reportDate
+   * @param to
+   *          latest reportDate
    * @return Collection of matching cases, never null
    */
   public Collection<ReportedCase> getCases(int areaTier, int caseTypeId,
       Date from, Date to);
 
   /**
-   * @param areaTier depth of AreaTypes in type hierarchie to match this case on
-   * @param from earliest reportDate
-   * @param to latest reportDate
+   * @param areaTier
+   *          depth of AreaTypes in type hierarchie to match this case on
+   * @param from
+   *          earliest reportDate
+   * @param to
+   *          latest reportDate
    * @return Collection of matching cases, never null
    */
   public Collection<ReportedCase> getCases(int areaTier, Date from, Date to);
+
+  /**
+   * Executes a crosstable query to count the number of cases, that occured at
+   * each area of specified tier. The cases are grouped by the specified
+   * Attribute.
+   * 
+   * @param attribute
+   *          Attribute by which the cases should be grouped
+   * @param areaTier
+   *          depth of AreaTypes in type hierarchie to match this case on
+   * @return Returns a Collection of Object arrays with each row containing
+   *         (count|attributevalue|area-identifier) ordered by
+   *         area-identifier,attributevalue.
+   */
+  public Collection<Object[]> countCasesPerAreaGroupedByAttribute(
+      String attribute, int areaTier);
 
   /**
    * @return earliest case in datastore by reportdate
@@ -48,10 +71,10 @@ public interface ReportedCaseDAO
    * @return number of cases in datastore
    */
   public long countCases();
-  
+
   /**
    * @return last change in case database
    */
   public Timestamp lastChange();
-  
+
 }
