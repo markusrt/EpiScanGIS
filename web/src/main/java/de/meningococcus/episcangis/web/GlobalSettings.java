f92c5d39066dfdd8264a698873754765c085fabf
Markus Reinhardt <tuxxilla@gmail.com>
2006-06-07 14:46:03 +0000

    ADD: central position for configfile access
    git-svn-id: svn://localhost/episcangis/trunk@156 878930ed-052c-0410-bf3c-f10d0cba850a

--- web/src/main/java/de/meningococcus/episcangis/web/GlobalSettings.java
+++ web/src/main/java/de/meningococcus/episcangis/web/GlobalSettings.java
@@ -1,8 +1,38 @@
 package de.meningococcus.episcangis.web;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public final class GlobalSettings
 {
+  private static Log log = LogFactory.getLog(GlobalSettings.class);
+  
   public static final String FORWARD_ERROR = "error",
   FORWARD_SUCCESS = "success", FORWARD_INDEX = "index", 
-  FORWARD_MAPBROWSER = "mapbrowser", MAPBROWSER_SITE_URL="mapbrowser.vm";
+  FORWARD_MAPBROWSER = "mapbrowser", MAPBROWSER_SITE_URL="mapbrowser.vm",
+  FORWARD_DENIED="denied";
+  
+  public static final String CONFIGFILE = "conf/epidegis-web.properties";
+  
+  public static String getConfigurationProperty(String property)
+      throws ConfigurationException
+  {
+    Configuration config;
+    String propertyValue = null;
+    try
+    {
+      config = new PropertiesConfiguration(CONFIGFILE);
+      propertyValue = config.getString(property, "");
+    }
+    catch (ConfigurationException e)
+    {
+      log.warn("Exception while loading " + CONFIGFILE + ": " + e.getMessage());
+      throw e;
+    }
+    return propertyValue;
+  }
+
 }
