99bb97acd4e4a7a5f4aa2ca06d48fd9413775dbb
Markus Reinhardt <tuxxilla@gmail.com>
2006-09-29 08:07:54 +0000

    ADD: unfinished experiment
    git-svn-id: svn://localhost/episcangis/trunk@274 878930ed-052c-0410-bf3c-f10d0cba850a

---
+++ web/src/main/java/de/meningococcus/episcangis/map/ExperimentalMap.java
@@ -0,0 +1,178 @@
+package de.meningococcus.episcangis.map;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.meningococcus.episcangis.db.DaoFactory;
+import de.meningococcus.episcangis.db.dao.AreaDAO;
+import de.meningococcus.episcangis.db.dao.AreaTypeDAO;
+import de.meningococcus.episcangis.db.dao.ReportedCaseDAO;
+import de.meningococcus.episcangis.db.model.Area;
+import de.meningococcus.episcangis.db.model.AreaType;
+
+/* ====================================================================
+ *   Copyright �2005 Markus Reinhardt - All Rights Reserved.
+ * ====================================================================
+ */
+
+public class ExperimentalMap extends AbstractWmsMap
+{
+  private static Log log = LogFactory.getLog(ExperimentalMap.class);
+
+  private AreaDAO aDao;
+
+  ParameterComponent fromAge, toAge, areaSelector, includeUnknownAge,
+      observationPeriod, fromMonth, toMonth;
+
+  public ExperimentalMap(int width, int height) throws MapInitializationException
+  {
+    super(width, height);
+    // Get DAOs for db access
+    DaoFactory daoFactory = DaoFactory.getDaoFactory();
+    AreaTypeDAO atDao = daoFactory.getAreaTypeDAO();
+    ReportedCaseDAO rcDao = daoFactory.getReportedCaseDAO();
+    aDao = daoFactory.getAreaDAO();
+
+    // Create AREA SelectParameter. Fill it with all areas, that are stored
+    // in db and are active
+    areaSelector = new SelectParameter("areaId", "Area");
+    for (AreaType at : atDao.getAreaTypes())
+    {
+      if (at.isActive())
+      {
+        Collection<Area> parentAreas = aDao.getAreas(at);
+        for (AreaType childAt : atDao.getChildAreaTypes(at.getId()))
+        {
+          StringBuilder sb = new StringBuilder();
+          for (int a = 0; a < childAt.getTier(); a++)
+          {
+            sb.append(' ');
+          }
+          Collection<Area> childAreas = aDao.getAreas(childAt);
+          Iterator<Area> childAreaIt = childAreas.iterator();
+          while (childAreaIt.hasNext())
+          {
+            Area childArea = childAreaIt.next();
+            String identifier = childArea.getIdentifier();
+            if (identifier.equals("Niedersachsen"))
+            {
+              childArea.setIdentifier("Bremen/Niedersachsen");
+            }
+            else if (identifier.equals("Schleswig-Holstein"))
+            {
+              childArea.setIdentifier("Hamburg/Schleswig-Holstein");
+            }
+            else if (identifier.equals("Rheinland-Pfalz"))
+            {
+              childArea.setIdentifier("Rheinland-Pfalz/Saarland");
+            }
+            else if (identifier.equals("Brandenburg"))
+            {
+              childArea.setIdentifier("Berlin/Brandenburg");
+            }
+
+            if (identifier.equals("Berlin") || identifier.equals("Hamburg")
+                || identifier.equals("Saarland") || identifier.equals("Bremen"))
+            {
+              childAreaIt.remove();
+            }
+            else
+            {
+              childArea
+                  .setIdentifier(sb.toString() + childArea.getIdentifier());
+            }
+          }
+          parentAreas.addAll(childAreas);
+        }
+
+        for (Area area : parentAreas)
+        {
+          ParameterValue val = new ParameterValue(area.getIdentifier(), String
+              .valueOf(area.getId()));
+          areaSelector.add(val);
+        }
+      }
+    }
+    addMapParameter(areaSelector);
+
+    // Create age range SelectParameters.
+    fromAge = new SelectParameter("fromAge", "Age from");
+    toAge = new SelectParameter("toAge", "to");
+    for (int age = 0; age <= 90; age++)
+    {
+      fromAge.add(new ParameterValue(String.valueOf(age), String.valueOf(age)));
+      ParameterValue toValue = new ParameterValue(String.valueOf(age), String
+          .valueOf(age));
+      if (age == 90)
+      {
+        toValue = new ParameterValue("90+", String.valueOf(1000));
+      }
+      toAge.add(toValue);
+    }
+    try
+    {
+      toAge.selectValue("1000");
+    }
+    catch (InvalidParameterValueException e){}
+
+    // TODO refactor this
+    // includeUnknownAge = new CheckboxParameter("incUAge", "include unknown
+    // age",
+    // new ParameterValue("yes", "-1"), new ParameterValue("no", "-2"));
+    // addMapParameter(includeUnknownAge);
+
+    addMapParameter(fromAge);
+    addMapParameter(toAge);
+
+    fromMonth = new ParameterValue("fromMonth", "2002-01-01");
+    toMonth = new ParameterValue("toMonth", "2006-12-31");
+    addMapParameter(fromMonth);
+    addMapParameter(toMonth);
+  }
+
+  public synchronized Collection<String> setParameter(String name, String value)
+      throws ParameterNotFoundException, InvalidParameterValueException
+  {
+    Collection<String> updatedLayers = new ArrayList<String>();
+    if(name.equals("fromMonth")) {
+      fromMonth = new ParameterValue(value);
+      fromMonth.setSelected(true);
+    }
+    else if(name.equals("toMonth")) {
+      toMonth = new ParameterValue(value);
+      toMonth.setSelected(true);
+    }
+    else {
+      updatedLayers.addAll(super.setParameter(name, value));
+    }
+    if (name.equals(areaSelector.getName()))
+    {
+      log.debug("AREAID: " + Integer.valueOf(value));
+      Area a = aDao.getArea(Integer.valueOf(value));
+      setBbox(a.getBoundingBox());
+    }
+    return updatedLayers;
+  }
+
+  public ParameterComponent getParameter(String name)
+  {
+    if(name.equals("fromMonth")) {
+      return fromMonth;
+    }
+    else if(name.equals("toMonth")) {
+      return toMonth;
+    }
+    else return super.getParameter(name);
+  }
+
+  protected MapLayerFactory getMapLayerFactory()
+  {
+    return new NrzmMapLayerFactory();
+  }
+
+}
---
+++ web/src/main/java/de/meningococcus/episcangis/map/ExperimentalMapLayerFactory.java
@@ -0,0 +1,78 @@
+package de.meningococcus.episcangis.map;
+
+import java.util.Locale;
+
+import de.meningococcus.episcangis.map.layer.BordersLayer;
+import de.meningococcus.episcangis.map.layer.CaseTypesLayer;
+import de.meningococcus.episcangis.map.layer.ClusterLayer;
+import de.meningococcus.episcangis.map.layer.IncidenceLayer;
+import de.meningococcus.episcangis.map.layer.MapLayer;
+import de.meningococcus.episcangis.map.layer.PopdensityLayer;
+import de.meningococcus.episcangis.map.layer.SerogroupsLayer;
+
+/* ====================================================================
+ *   Copyright �2005 Markus Reinhardt - All Rights Reserved.
+ * ====================================================================
+ */
+
+final class ExperimentalMapLayerFactory implements MapLayerFactory
+{
+  public MapLayer getMapLayer(String name, String title, boolean hasLegend,
+      AbstractWmsMap map)
+  {
+    MapLayer ret;
+    String realName;
+    if( name == null ) {
+      name = "<unnamed layer>";
+    }
+    int index = name.lastIndexOf('_');
+    if (index != -1)
+    {
+      realName = name.substring(0, index).toLowerCase(Locale.ENGLISH);
+    }
+    else
+    {
+      realName = name;
+    }
+    if (realName.equals("casetypes"))
+    {
+      ret = new CaseTypesLayer(name, title, hasLegend, map);
+    }
+    else if (realName.equals("serogroups"))
+    {
+      ret = new SerogroupsLayer(name, title, hasLegend, map);
+    }
+    else if (realName.equals("incidence"))
+    {
+      ret = new IncidenceLayer(name, title, hasLegend, map);
+    }
+    else if (realName.equals("borders"))
+    {
+      ret = new BordersLayer(name, title, hasLegend, map);
+    }
+    else if (realName.equals("popdensity"))
+    {
+      ret = new PopdensityLayer(name, title, hasLegend, map);
+    }
+    else if (realName.equals("cluster"))
+    {
+      //TODO remove ids (82)
+      ret = new ClusterLayer(name, title, hasLegend, true, map, 82);
+    }
+    else if (realName.equals("clusterretro"))
+    {
+      //TODO remove ids (211)
+      ret = new ClusterLayer(name, title, hasLegend, true, map, 211);
+    }
+    else
+    {
+      ret = new MapLayer(name, title, hasLegend, map);
+    }
+    return ret;
+  }
+
+  public String getMapUrlKeyPrefix()
+  {
+    return "experimental.";
+  }
+}
