d9336834430b193ee0ff2fdb44255bdaf7d69d3d
Markus Reinhardt <tuxxilla@gmail.com>
2007-11-06 13:10:03 +0000

    FIX: NullpointerException on empty case lists
    git-svn-id: svn://localhost/episcangis/trunk@2725 878930ed-052c-0410-bf3c-f10d0cba850a

--- web/src/main/java/de/meningococcus/episcangis/web/GlobalInformation.java
+++ web/src/main/java/de/meningococcus/episcangis/web/GlobalInformation.java
@@ -1,27 +1,37 @@
 package de.meningococcus.episcangis.web;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 import de.meningococcus.episcangis.db.DaoFactory;
 import de.meningococcus.episcangis.db.dao.ReportedCaseDAO;
+import de.meningococcus.episcangis.db.model.ReportedCase;
 
-public class GlobalInformation
-{
-  ReportedCaseDAO rcDao;
-  public GlobalInformation() {
-    rcDao = DaoFactory.getDaoFactory().getReportedCaseDAO();
-  }
-  
-  public long getCaseCount() {
-    return rcDao.countCases();
-  }
-  
-  public Date getLatestCaseDate() {
-    return rcDao.getLatestCase().getReportDate();
-  }
-  
-  public Date getLastChange() {
-    return new Date(rcDao.getLastChange().getTime());
-  }
+public class GlobalInformation {
+	ReportedCaseDAO rcDao;
+
+	public GlobalInformation() {
+		rcDao = DaoFactory.getDaoFactory().getReportedCaseDAO();
+	}
+
+	public long getCaseCount() {
+		return rcDao.countCases();
+	}
+
+	public Date getLatestCaseDate() {
+		ReportedCase rCase = rcDao.getLatestCase();
+		if (rCase != null) {
+			return rCase.getReportDate();
+		}
+		return null;
+	}
+
+	public Date getLastChange() {
+		Timestamp lastChange = rcDao.getLastChange();
+		if (lastChange != null) {
+			return new Date(lastChange.getTime());
+		}
+		return null;
+	}
 
 }
